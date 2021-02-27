db7e6c67951ad72050b69c7a9ce9df0dcb43a6bc
john roque jorillo <jorillojohnroque@gmail.com>
2021-02-27 20:38:58 +0800

    Add NSPersistentContainer load helper function

---
+++ FeedStoreChallenge/CoreDataFeedStore/Helper/NSPersistentContainer+load.swift
@@ -0,0 +1,33 @@
+//
+//  NSPersistentContainer+load.swift
+//  FeedStoreChallenge
+//
+//  Created by John Roque Jorillo on 2/27/21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+
+import CoreData
+
+internal extension NSPersistentContainer {
+	static func load(modelName: String, storeURL: URL, in bundle: Bundle) throws -> NSPersistentContainer {
+		guard let modelURL = bundle.url(forResource: modelName, withExtension: "momd"),
+			  let model = NSManagedObjectModel(contentsOf: modelURL) else {
+			throw LoadError.invalidModel
+		}
+		
+		let description = NSPersistentStoreDescription(url: storeURL)
+		let container = NSPersistentContainer(name: modelName, managedObjectModel: model)
+		container.persistentStoreDescriptions = [description]
+		var loadError: Swift.Error?
+		container.loadPersistentStores { loadError = $1 }
+		try loadError.map { _ in throw LoadError.unableToLoadPersistentStore }
+		return container
+	}
+}
+
+public extension NSPersistentContainer {
+	enum LoadError: Swift.Error {
+		case invalidModel
+		case unableToLoadPersistentStore
+	}
+}
